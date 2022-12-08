724405299b582c110c1ba72c8e408da21aff1db8
MFlisar <mflisar85@gmail.com>
2022-12-08 13:47:20 +0100

    BooleanDependency extended: allows to define true/false dependency now

--- library-preference-screen/src/main/java/com/michaelflisar/materialpreferences/preferencescreen/dependencies/BooleanDependency.kt
+++ library-preference-screen/src/main/java/com/michaelflisar/materialpreferences/preferencescreen/dependencies/BooleanDependency.kt
@@ -3,11 +3,12 @@ package com.michaelflisar.materialpreferences.preferencescreen.dependencies
 import com.michaelflisar.materialpreferences.core.interfaces.StorageSetting
 
 class BooleanDependency(
-    override val setting: StorageSetting<Boolean>
+    override val setting: StorageSetting<Boolean>,
+    val dependOnTrue: Boolean = true
 ) : Dependency<Boolean> {
 
     override suspend fun state(): Boolean {
-        return setting.read()
+        return setting.read().let { if (dependOnTrue) it else !it }
     }
 }
 
